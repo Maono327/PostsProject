671d444d62da82774c4a09eae8824d40379ccbe9
Maono327 <maono3300@gmail.com>
2025-09-19 00:00:29 +0300

    [Feat] Add Spring Boot skeleton

---
+++ src/main/java/com/maono/bootpostsproject/BootPostsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.maono.bootpostsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPostsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootPostsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maono/bootpostsproject/BootPostsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maono.bootpostsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootPostsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
