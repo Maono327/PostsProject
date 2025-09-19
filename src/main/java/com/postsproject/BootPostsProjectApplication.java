05507495b0cf0cc3bca0e83bb08c4082c63136d1
Maono327 <maono3300@gmail.com>
2025-09-19 11:27:37 +0300

    [Feat] Change common structure

--- src/main/java/com/postsproject/BootPostsProjectApplication.java
+++ src/main/java/com/postsproject/BootPostsProjectApplication.java
@@ -1,4 +1,4 @@
-package com.maono.bootpostsproject;
+package com.postsproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
